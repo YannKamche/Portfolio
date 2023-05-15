a879fe7bb9a2083ecd598ae0785af3578011c158
Yann Kamche <yannkamche@gmail.com>
2023-05-15 13:01:39 +0100

    Added the app.jsx file

---
+++ my-portfolio/src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div>App</div>
+  )
+}
+
+export default App 
+
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+reactDom.render(<APP/> , document.getElementById('root')); //THIS LINE INDICATE THAT
+// ALL THE CONTENT OF THIS FILE SHOULD BE RENDERED IN THE ROOT ID OF THE HTML FILE
