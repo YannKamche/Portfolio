10b9bd72ae92dc8c4b2a4cafea68900087b8b668
Yann Kamche <yannkamche@gmail.com>
2023-05-18 14:00:51 +0100

    Edited the code

--- my-portfolio/src/App.js
+++ my-portfolio/src/App.js
@@ -1,25 +1,40 @@
-import logo from './logo.svg';
+
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+// function App() {
+//   return (
+//     <div className="App">
+//     <h1>Hello World</h1>
+//     </div>
+//   );
+// }
 
-export default App;
+// export default App;
+
+import Header from './component/header/Header';
+import Navbar from './component/navbar/Navbar';
+import About from './component/about/About';
+import Experience from './component/experience/Experience';
+import Services from './component/services/Services';
+import Portfolio from './component/portfolio/Portfolio';
+import Testimonial from './component/testimonial/Testimonial';
+import Contact from './component/contact/Contact';
+import Footer from './component/footer/Footer';
+
+function App() {
+return (
+<div>
+  <Header />
+  <Navbar />
+  <About />
+  <Experience />
+  <Services />
+  <Portfolio />
+  <Testimonial />
+  <Contact />
+  <Footer />
+</div>
+
+);
+}
+export default App; 
--- my-portfolio/src/component/Contact/Contact.jsx
+++ my-portfolio/src/component/Contact/Contact.jsx
@@ -1,15 +1,24 @@
+// import React from 'react'
+
+// export default function Contact() {
+//   return (
+//     <div>Contact</div>
+//   )
+// }
+
 import React from 'react'
 import './contact.css'
 import {MdOutlineEmail} from 'react-icons/md'
 import {AiOutlineLinkedin} from 'react-icons/ai'
 import {BsWhatsapp} from 'react-icons/bs'
 import {useRef} from 'react'
-import emailjs from 'emailjs-com'
+
 const Contact = () => {
 const form = useRef();
-const sendEmail = (e) => {e.preventDefault();
-emailjs.sendForm('service_5vuauza', 'template_fdnnl56', form.current,
-'EmSdUNQzQuxkUqGWx')
+const sendEmail = (e) => {
+e.preventDefault();
+// emailjs.sendForm('service_5vuauza', 'template_fdnnl56', form.current,
+// 'EmSdUNQzQuxkUqGWx')
 e.target.reset()
 .then((result) => {
 console.log(result.text)
@@ -18,6 +27,7 @@ console.log(error.text);
 });
 };
 return (
+
 <section id="contact">
 <h5>Get in touch</h5>
 <h2>Contact me</h2>
@@ -25,32 +35,47 @@ return (
 <div className="contact__options">
 <article className='contact__option'>
 <MdOutlineEmail className='contact__option__icon'/>
+
 <h4>Email</h4>
-<h5>myEmail@gmail.com</h5>
-<a href='mailto:aldenovpoutine99@gmail.com' target='_blank'>send a
-message</a>
+<h5>nyandoonotex@gmail.com</h5>
+<a href='mailto:nyandoonotex@gmail.com' target='_blank' rel='' >send a message</a>
 </article>
 <article className='contact__option'>
 <AiOutlineLinkedin className='contact__option__icon'/>
+
 <h4>LinkedIn</h4>
 <h5>Profile</h5>
-<a href='https://www.linkedin.com/in/brown-djomo-844b96164/'
-target='_blank'>Tape me on LinkedIn</a>
+<a href='https://www.linkedin.com/in/nyando-onongwene-321210228/' rel=''
+target='_blank' >Tap me on LinkedIn</a>
 </article>
 <article className='contact__option'>
 <BsWhatsapp className='contact__option__icon'/>
+
 <h4>Whatsapp</h4>
 <h5>Direct message</h5>
-<a href='https://web.whatsapp.com/send?phone=+237657268549'
+<a href='https://web.whatsapp.com/send?phone=+237675831475' rel=''
 target='_blank'>Whatsapp me</a>
 </article>
 </div>
+
 <form ref={form} onSubmit={sendEmail}>
+ <div className='Name'>  
 <input type="text" name='name' placeholder='full name' required/>
+</div> 
+
+<div className='Email'>
 <input type="email" name='email' placeholder='your email' required/>
+</div>
+
+<div className='Message'>
 <textarea name="message" id="message" cols="30" rows="10"
 placeholder='your message' required></textarea>
-<button type="submit" className='btn btn-primary'>send message</button></form>
+</div>
+
+<div className='submit-Btn'>
+<button type="submit" className='btn btn-primary'>send message</button>
+</div>
+</form>
 </div>
 </section>
 )
