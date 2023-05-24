15f305f83a2f2f1f4cc251d83a0cfecf318b506b
Yann Kamche <yannkamche@gmail.com>
2023-05-24 16:14:18 +0100

    Final editing

--- my-portfolio/src/component/header/HeaderSocial.jsx
+++ my-portfolio/src/component/header/HeaderSocial.jsx
@@ -1,3 +1,4 @@
+
 import React from 'react'
 import {BsLinkedin} from 'react-icons/bs'
 import {FaGithub} from 'react-icons/fa'
@@ -5,7 +6,7 @@ import {FaFacebookF} from 'react-icons/fa'
 import {CgTwitter} from 'react-icons/cg'
 const HeaderSocial = () => {
 return (
-<div className='header__social'>
+<div className='header_social'>
 <a href='https://linkedin.com' target='blank'><BsLinkedin/> </a>
 <a href='https://github.com' target='blank'><FaGithub/> </a>
 <a href='https://facebook.com' target='blank'><FaFacebookF/> </a>
@@ -13,4 +14,4 @@ return (
 </div>
 )
 }
-export default HeaderSocial
+export default HeaderSocial;
